467b9de3c0a590e2ea26e60974049f26b08f50ec
Lope <Lope922@gmail.com>
2015-10-28 00:02:48 -0500

    rought draft with working departure and return dates. see app description for things to do or improve on.

--- Lab 10/Form1.Designer.cs
+++ Lab 10/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Lab_10
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         /// Required designer variable.
@@ -29,25 +29,35 @@
         private void InitializeComponent()
         {
             this.btnMap = new System.Windows.Forms.Button();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerDeparture = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePickerReturnDate = new System.Windows.Forms.DateTimePicker();
+            this.listBoxTripDatesOrData = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnEnterDates = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnMap
             // 
+            this.btnMap.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnMap.Location = new System.Drawing.Point(122, 43);
             this.btnMap.Name = "btnMap";
             this.btnMap.Size = new System.Drawing.Size(75, 23);
             this.btnMap.TabIndex = 0;
             this.btnMap.Text = "Map";
             this.btnMap.UseVisualStyleBackColor = true;
+            this.btnMap.Click += new System.EventHandler(this.btnMap_Click);
             // 
-            // dateTimePicker1
+            // dateTimePickerDeparture
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(38, 177);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePickerDeparture.Location = new System.Drawing.Point(38, 177);
+            this.dateTimePickerDeparture.Name = "dateTimePickerDeparture";
+            this.dateTimePickerDeparture.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerDeparture.TabIndex = 1;
             // 
             // label1
             // 
@@ -58,16 +68,97 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Pick your route";
             // 
-            // Form1
+            // progressBar1
             // 
+            this.progressBar1.Location = new System.Drawing.Point(492, 291);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Departure Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(276, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Return Date";
+            // 
+            // dateTimePickerReturnDate
+            // 
+            this.dateTimePickerReturnDate.Location = new System.Drawing.Point(292, 177);
+            this.dateTimePickerReturnDate.Name = "dateTimePickerReturnDate";
+            this.dateTimePickerReturnDate.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerReturnDate.TabIndex = 6;
+            // 
+            // listBoxTripDatesOrData
+            // 
+            this.listBoxTripDatesOrData.FormattingEnabled = true;
+            this.listBoxTripDatesOrData.Location = new System.Drawing.Point(168, 206);
+            this.listBoxTripDatesOrData.Name = "listBoxTripDatesOrData";
+            this.listBoxTripDatesOrData.Size = new System.Drawing.Size(305, 95);
+            this.listBoxTripDatesOrData.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(61, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Planned Trip Dates:";
+            // 
+            // btnEnterDates
+            // 
+            this.btnEnterDates.Location = new System.Drawing.Point(508, 177);
+            this.btnEnterDates.Name = "btnEnterDates";
+            this.btnEnterDates.Size = new System.Drawing.Size(75, 108);
+            this.btnEnterDates.TabIndex = 9;
+            this.btnEnterDates.Text = "Enter Dates";
+            this.btnEnterDates.UseVisualStyleBackColor = true;
+            this.btnEnterDates.Click += new System.EventHandler(this.btnEnterDates_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(12, 278);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(45, 23);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "&Esc";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Main
+            // 
+            this.AcceptButton = this.btnEnterDates;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(444, 351);
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(595, 313);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnEnterDates);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBoxTripDatesOrData);
+            this.Controls.Add(this.dateTimePickerReturnDate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dateTimePickerDeparture);
             this.Controls.Add(this.btnMap);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "Main";
+            this.Text = "Travel App";
+            this.Load += new System.EventHandler(this.Main_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -76,8 +167,16 @@
         #endregion
 
         private System.Windows.Forms.Button btnMap;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDeparture;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePickerReturnDate;
+        private System.Windows.Forms.ListBox listBoxTripDatesOrData;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnEnterDates;
+        private System.Windows.Forms.Button btnExit;
 
     }
 }
--- Lab 10/Form1.cs
+++ Lab 10/Form1.cs
@@ -10,16 +10,143 @@ using System.Windows.Forms;
 
 namespace Lab_10
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        /* THIS PROGRAM WILL AID IN PLANNING YOU TRAVEL 
+         
+         * * IT CAN SUGGEST WEATHER 
+         *      SUGGEST LIST OF ITEMS TO BRING
+         *      EMERGENCY ROAD SIDE ASSITANCE NUMBER 
+         *  //RADAR GIF -- EXTRA FEATURE 
+         *  
+         * // MAP -- ALREADY DONE 
+         * 
+         * // HOTEL -- DRAWN FROM LIST BY REGION. 
+         * 
+         * // GAS CALCULATOR 
+         * 
+         * // MILEAGE LOG 
+         /*
+         * /
+        */
+
+        //todo will need to tie in hardcoded data or enum for region weather information. 
+       
+        // public values ( might later be added to structure of class) 
+ 
+       public DateTime dateout;
+       public DateTime dateIn; 
+
+
+        private void btnMap_Click(object sender, EventArgs e)
         {
+            // pop up a dialog window 
+            callPopUPWindow();
 
         }
+
+        //todo add code and design the dialog window. 
+        // this window in particular is the map to have checkboxes to select a region. 
+        private void callPopUPWindow()
+        {
+             throw new NotImplementedException();
+        }
+
+
+        // closes the main window 
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        //todo when this button is clicked record the arrival and departure date for the trip 
+        private void btnEnterDates_Click(object sender, EventArgs e)
+        {
+            // set these two days first then test them 
+            DateTime dept  = dateTimePickerDeparture.Value;
+            
+            DateTime comeBack = dateTimePickerReturnDate.Value;
+
+        if  (validateDateInput(dept, comeBack)  == true)
+        {
+            // call the methods to find the departure and return date 
+            getSetPickedTripDates(dateTimePickerDeparture, dateTimePickerReturnDate);
+
+           listBoxTripDatesOrData.Items.Add("Depature Date: " + dept.ToShortDateString() + "Return Date:" + comeBack.ToShortDateString());
+            // pass down dept and comeback so that to a single method to determine both. 
+
+           progressBar1.Value = (10); 
+        }
+        }
+
+
+        // first write a method to validate data input based off of current date 
+
+        // this method requires the data to be set already when passed in. 
+        public bool validateDateInput(DateTime deptartureDate, DateTime returnDateTime)
+        {
+            try
+            {
+                // set the current dateTime to most recent value 
+                DateTime thisCurrentRunningSessionDateAndtime = DateTime.Now;
+
+
+                if (deptartureDate >= thisCurrentRunningSessionDateAndtime)
+                {
+                    // then do this 
+                    MessageBox.Show("Check your dates you cannot depart back in time", " Hey McFly!");
+
+                    // throw the flag 
+                    return false;
+                }
+                else if (returnDateTime.AddSeconds(10) <= deptartureDate)
+                {
+                    MessageBox.Show("You cannot return before you depart. \n How? \n how do you expect to accomplish this ?", "How?");
+                    // throw the flag 
+                    return false;
+                }
+                return true;
+            }
+            // catch any crazy errors. 
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something went terribly wrong here while trying to retrieve the current date and time." + ex.Message + "Strack Trace:" + ex.StackTrace, "Uh, Oh");
+            }
+
+            // return true on all paths and hope of the best ! 
+            return true; 
+
+        }
+
+        // next write the method to set date and time
+        public void getSetPickedTripDates(DateTimePicker go, DateTimePicker comeback)
+        {
+            //verbose
+             dateout =  Convert.ToDateTime(go.Value);
+             dateIn = Convert.ToDateTime(comeback.Value);
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            DateTime tomrrow; 
+           
+            // set the return trip for at least tomorrow 
+         //  tomrrow = DateTime.Now.AddDays(1);
+           dateTimePickerReturnDate.Value.AddDays(1); 
+        }
+
+
+                
+        }
+
     }
-}
+
+        
+    
+        
